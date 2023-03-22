315cfb16bda06636643548d2491e8597bd309fda
Pietromuresu <muresupietro66@gmail.com>
2023-03-22 11:23:39 +0100

    add event click to buttons inside the form

--- Assets/scripts.js
+++ Assets/scripts.js
@@ -1,61 +1,63 @@
+const input2 = document.getElementById('kmToDo')
+  const select = document.getElementById('majority')
+  const name = document.getElementById('name2')
+  console.log(name); 
+generate.addEventListener('click', function() {
+
+
+  
+
+
+const priceForKm = 0.21 ; 
+const fasciaEta = select.value;
+const naming = name.value;
+let km = input2.value;
+console.log(km, fasciaEta);
+
+let offerta = 'Prezzo Standard',
+   sconto = 0,
+   prezzo;
+
+
+
+ if (fasciaEta === 'minorenne') {
+   sconto = 20;
+   offerta = 'sconto minorenni';
+ }else if (fasciaEta === 'over-65'){
+   sconto = 40; 
+   offerta = 'sconto over-65';
+
+ }
+
+
+  prezzo = (km * priceForKm) * (1 - sconto/100);
+ console.log( prezzo);
+
+document.getElementById('output').innerHTML = naming;
+ document.getElementById('outputKmToDo').innerHTML = km;
+ document.getElementById('totalPrice').innerHTML = prezzo.toFixed(2);
+ document.getElementById('offer').innerHTML = offerta;
  
- const priceForKm = 0.21 ; 
- const input2 = document.getElementById('kmToDo')
- const select = document.getElementById('majority')
-const minorenne = document.getElementById('minorenne')
-const maggiorenne = document.getElementById('maggiorenne')
- console.log(name);
 
- generate.addEventListener('click', function(){
-  const input = document.getElementById('name');
-  const name = input.value;
-  output.innerHTML = name;
-  console.log(input);
- })
+});
 
- generate.addEventListener('click', function(){
-   
-    let input2 = document.getElementById('kmToDo');
-    
-    let kmToDo = Number(input2.value);console.log(kmToDo)
-    outputKmToDo.innerHTML = kmToDo + '' + 'Km';
-    console.log(input2);
-    
-     
-  })
 
-  generate.addEventListener('click', function() {
 
-    let kmToDo = Number(input2.value);
-    console.log(kmToDo)
-    let price = (priceForKm * parseInt(kmToDo));
-    console.log(price);
-    document.getElementById('totalPrice').innerHTML = `
-      ${price} &euro;
-      `;
-  if (select.selectedIndex.value = 1){
-      
-      let price2 = price *= 1 - (20/100);
-      console.log(price2);
-      document.getElementById('totalPrice').innerHTML = `
-      ${price2} &euro;
-      `;
-  
-    } else if (select.selectedIndex.value = 2 ) {
-      let price3 = (price *= 1 - (40/100));
-      console.log(price3);
-      document.getElementById('totalPrice').innerHTML = `
-      ${price3} &euro;
-      `;
-    }
-   
-  
 
-  } )
+annulla.addEventListener('click', function() {
+name.value = '';
+kmToDo.value = '';
+majority.value = '----'
 
-  
+})
+
+
+
+
 
  
+
+
   
 
   
